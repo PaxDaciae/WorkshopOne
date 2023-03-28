d80a2948eed70c6ca675a96ae90a89531471fe5b
Alexandru Micu <80334035+PaxDaciae@users.noreply.github.com>
2023-03-29 00:00:21 +0300

    Update ClickCube.jsx
    To do: add comments as structure of code is no longer simple and I need to keep track of what I'm doing.

--- fidgetOne/Components/ClickCube.jsx
+++ fidgetOne/Components/ClickCube.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import {useState} from 'react'
 export default function Cube(props){
     
-    const cubeStyle = {
+    const [cubeState, setCubeState] = useState(0)
+    function alertYes(){
+        console.log("yes")
+    }
+    const [cubeStyle, setCubeStyle] = useState({
         height: "100px",
         width: "100px",
         border: "1px solid black",
@@ -10,13 +14,32 @@ export default function Cube(props){
         margin: "auto",
         borderRadius: "5px",
         backgroundColor: "#F8F3C3"
+    })
+
+    function cubeColorChange(){
+        console.log("CCHAN")
+        if ((cubeStyle.backgroundColor = "#F8F3C3")&&(props.count%3 === 0)){
+            setCubeStyle(prevStyle => ({...cubeStyle, backgroundColor: "#FF0000"}))
+        } else {
+            setCubeStyle(prevStyle => ({...cubeStyle, backgroundColor: "#F8F3C3"}))
+        }
+    }
+
+    function clickEvent(props){
+        props.handleClick()
+        console.log(typeof(props), props.count)
     }
 
     return(
-        <div style = {cubeStyle} onClick = {props.handleClick} className = "testCube">
+        <div style = {cubeStyle} onClick = {() => {props.handleClick(), cubeColorChange()}} className = "testCube">
             {/* {(props.count <= 3) ? props.count : props.count%4}
             <br></br> */}
             {props.count}
+            <br></br>
+            
         </div>
     )
 }
+
+// function(){return 
+//     cubeColorChange
