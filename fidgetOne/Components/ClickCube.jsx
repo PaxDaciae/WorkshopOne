e1da01d4c929d3ea5abd7e3fe68bc46531df95e6
Alexandru Micu <80334035+PaxDaciae@users.noreply.github.com>
2023-03-29 12:43:38 +0300

    Sidegrade work

--- fidgetOne/Components/ClickCube.jsx
+++ fidgetOne/Components/ClickCube.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import {useState} from 'react'
+import Squirrel from "../src/Images/Squirrel.png"
+
+
 export default function Cube(props){
     
     const [cubeState, setCubeState] = useState(0)
     function alertYes(){
-        console.log("yes")
+        alert("yes")
     }
+
     const [cubeStyle, setCubeStyle] = useState({
         height: "100px",
         width: "100px",
@@ -13,33 +17,48 @@ export default function Cube(props){
         // display: "inline-block",
         margin: "auto",
         borderRadius: "5px",
-        backgroundColor: "#F8F3C3"
-    })
+        backgroundColor: "#F8F3C3",
+        userSelect: "none"
+    })    
 
-    function cubeColorChange(){
-        console.log("CCHAN")
-        if ((cubeStyle.backgroundColor = "#F8F3C3")&&(props.count%3 === 0)){
-            setCubeStyle(prevStyle => ({...cubeStyle, backgroundColor: "#FF0000"}))
-        } else {
-            setCubeStyle(prevStyle => ({...cubeStyle, backgroundColor: "#F8F3C3"}))
-        }
+    const buttonStyle = {
+        width: "100px",
+        height: "100px",
+        backgroundColor: "yellow",
+        ImageBackground: `url(${Squirrel})`
     }
-
-    function clickEvent(props){
-        props.handleClick()
-        console.log(typeof(props), props.count)
-    }
+    
+    // function clickEvent(props){
+    //     props.handleClick()
+    //     function cubeColorChange(){
+        
+    //         if (cubeStyle.backgroundColor === "#F8F3C3" && props.count === 2){
+    //             setCubeStyle(prevStyle => ({...prevStyle, backgroundColor: "#FF0000"}))
+    //         } else {
+    //             setCubeStyle(prevStyle => ({...prevStyle, backgroundColor: "#F8F3C3"}))
+    //         }
+    //     }
+    //     cubeColorChange()
+    // }
 
     return(
-        <div style = {cubeStyle} onClick = {() => {props.handleClick(), cubeColorChange()}} className = "testCube">
-            {/* {(props.count <= 3) ? props.count : props.count%4}
-            <br></br> */}
+        <div>
+            <a onClick = {props.handleClick}><img src = {Squirrel}></img></a>
+            <hr></hr>
             {props.count}
-            <br></br>
-            
+            {props.count === 3 ? alertYes() : ""}
         </div>
+        // <div style = {cubeStyle} onClick = {() => {clickEvent(props)}} className = "testCube">
+        //     {/* {(props.count <= 3) ? props.count : props.count%4}
+        //     <br></br> */}
+        //     {props.count}
+        //     <br></br>
+        //     {(props.count === 3)? <p>Testy</p> : <p>noTesty</p>}
+        // </div>
     )
 }
 
 // function(){return 
 //     cubeColorChange
+
+// style = {buttonStyle} 
